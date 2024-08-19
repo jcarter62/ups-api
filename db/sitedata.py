76094242350a73b665cdfe67f9ff8716c64fb9f1
Jim Carter <jcarter62@gmail.com>
2024-08-19 16:48:17 -0700

    add graphic

--- db/sitedata.py
+++ db/sitedata.py
@@ -53,21 +53,49 @@ class SiteData:
         self.cursor.execute('DELETE FROM data WHERE timestamp < ? ', (timestamp,))
         self.conn.commit()
 
-    def get_data(self, host, days=1):
+    def get_data(self, days=1):
         timestamp = datetime.datetime.now() - datetime.timedelta(days=days)
         timestamp = timestamp.isoformat()
-        self.cursor.execute('SELECT * FROM data WHERE IP = ? AND timestamp > ? ', (host, timestamp,))
+        self.cursor.execute('SELECT * FROM data WHERE timestamp > ? ', (timestamp,))
         return self.cursor.fetchall()
 
-    def get_24hrs_data(self, host):
-        data = self.get_data(host, days=1)
+    def get_24hrs_data(self):
+        data = self.get_data(days=1)
         return data
 
-    def get_7days_data(self, host):
-        data = self.get_data(host, days=7)
+    def get_7days_data(self):
+        data = self.get_data(days=7)
         return data
 
-    def get_most_recent_data(self, host):
-        self.cursor.execute('SELECT * FROM data WHERE IP = ? ORDER BY timestamp DESC LIMIT 1', (host,))
-        return self.cursor.fetchone()
+    def get_most_recent_data(self):
+        sqlcmd = 'SELECT * FROM data ORDER BY timestamp DESC LIMIT 1'
+        self.cursor.execute(sqlcmd)
+        rslt = self.cursor.fetchone()
+        return rslt
 
+
+    def get_average_hourly(self, days: int = 1):
+        timestamp = datetime.datetime.now() - datetime.timedelta(days=days)
+        timestamp = timestamp.isoformat()
+
+        self.cursor.execute('''
+            SELECT strftime('%Y-%m-%d %H', timestamp) as hour, 
+                   AVG(temp_f) as avg_temp, 
+                   MAX(temp_f) as max_temp, 
+                   MIN(temp_f) as min_temp
+            FROM data 
+            WHERE timestamp > ? 
+            GROUP BY hour
+            ORDER BY hour
+        ''', (timestamp,))
+
+        rslts = []
+        for row in self.cursor.fetchall():
+            rslts.append({
+                'hour': row[0],
+                'avg_temp': f"{row[1]:.2f}",
+                'max_temp': f"{row[2]:.2f}",
+                'min_temp': f"{row[3]:.2f}"
+            })
+
+        return rslts
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from uuid import uuid1
+
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse, FileResponse
 from dotenv import load_dotenv
 import os
 from db import SystemsDB, admin, SiteData
-from snmpread import Sites
+from snmpread import Sites, Site
 
 app = FastAPI()
 
@@ -16,14 +18,22 @@ approot = os.environ.get('APP_ROOT', '')
 template_folder = os.path.join(approot, "templates")
 templates = Jinja2Templates(directory= template_folder)
 
-
 @app.get("/")
 async def root(request: Request):
     systems = Sites()
+    data = []
+    n = 100
     for site in systems.sites:
-        site.__load_data__()
-    return templates.TemplateResponse("home.html", {"request": request, "sites": systems.sites})
-
+        n += 3
+        id = "id" + str(n).zfill(3)
+        data.append({
+            "id": id,
+            "host": site.host,
+            "description1": "",
+            "description2": "",
+            "temperature_f": ""
+        })
+    return templates.TemplateResponse("home.html", {"request": request, "sites": data})
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
@@ -38,15 +48,12 @@ async def initdb():
     db.close()
     return {"message": "Database initialized"}
 
-
 @app.get("/sites")
 async def sites():
     systems = Sites()
     for site in systems.sites:
         site.__load_data__()
-
     return {"systems": [site.__dict__ for site in systems.sites] }
-
 
 @app.get("/status", response_class=HTMLResponse)
 async def status(request: Request):
@@ -67,9 +74,29 @@ async def log_sites():
         db.insert_record(host=site.host, temp_f=site.temperature_f)
         db.close()
         db = None
-
     return {"message": "Data logged"}
 
+@app.get("/data/get-latest/{host}")
+async def get_latest(host: str):
+    db = SiteData(host=host)
+    dat = db.get_most_recent_data()
+    db.close()
+
+    sites = Sites()
+    data = []
+    for oneSite in sites.sites:
+        if oneSite.host == host:
+            oneSite.__load_data__()
+            data = [dat[0], dat[1], oneSite.description2, oneSite.description1, oneSite.temperature_f]
+            break
+    return {"data": data}
+
+@app.get("/data/get-latest-day/{host}")
+async def get_latest_day(host: str):
+    db = SiteData(host=host)
+    data = db.get_average_hourly(days=1)
+    db.close()
+    return {"data": data}
 
 @app.get("/favicon.ico", include_in_schema=False)
 async def favicon():
@@ -80,3 +107,24 @@ async def images(image_name: str):
     return FileResponse(os.path.join(approot, "images", image_name))
 
 
+@app.get("/site-graph/{host}")
+async def site_graph_data(request: Request, host: str):
+    db = SiteData(host=host)
+    data = db.get_average_hourly(days=7)
+    db.close()
+
+
+
+    # prepare data for graph
+    sitename = host
+
+    labels = []
+    datavalues = []
+    for one in data:
+        labels.append(one['hour'])
+        datavalues.append(one['avg_temp'])
+
+    return templates.TemplateResponse(
+        "site-graph.html",
+        {"request": request, "site": sitename, "labels":labels, "data":datavalues}
+    )
